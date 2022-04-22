66403bd07c9656297c84c5541b221b7c4030dc36
John Johansen <johnjohansendev@gmail.com>
2022-04-21 23:20:23 -0400

    CRA with MUI boilerplate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
 function App() {
   return (
-    <div>
-      <h1>CRA BASE BP</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Container>
+        <Box sx={{ bgcolor: 'none', height: '100vh' }}>
+          <Typography variant="h2" align="center">
+            CRA with MUI boilerplate
+          </Typography>
+        </Box>
+      </Container>
+    </>
   );
 }
 
